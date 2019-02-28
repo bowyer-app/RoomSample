60ff2630730f69d13c2aa15e63dbf06220671d27
bowyer-app <bowyer.app@gmail.com>
2019-02-28 09:38:38 +0900

    Open AdviceRegisterActivity

--- app/src/main/java/com/bowyer/app/roomsample/MainActivity.kt
+++ app/src/main/java/com/bowyer/app/roomsample/MainActivity.kt
@@ -2,17 +2,21 @@ package com.bowyer.app.roomsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.bowyer.app.roomsample.database.AppDatabase
-import javax.inject.Inject
+import androidx.databinding.DataBindingUtil
+import com.bowyer.app.roomsample.databinding.ActivityMainBinding
+import com.bowyer.app.roomsample.ui.activity.AdviceRegisterActivity
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var appDatabase: AppDatabase
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RoomApplication.component(this).inject(this)
-        setContentView(R.layout.activity_main)
+        binding.fab.setOnClickListener {
+            AdviceRegisterActivity.startActivity(this, null)
+        }
     }
 }
